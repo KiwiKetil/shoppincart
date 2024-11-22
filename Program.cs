adb9c969ea314d5268b23f87accdd9088088bf41
KiwiKetil <ketilsveberg@gmail.com>
2024-11-22 12:32:33 +0100

    first

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using shoppingcart;
+
+Item item = new("greenCheese", 15.25m, 18);
+Item item2 = new("SoftDrink", 2.99m, 7);
+
+ShoppingCart cart = new();
+ShoppingCart cart2 = new();
+
+cart.AddItem(item);
+cart.AddItem(item2);
+cart.AddItem(item2);
+cart.AddItem(item2);
+
+cart.DisplayCartItems();
+
+cart.Checkout();
+cart2.Checkout();
---
+++ ShoppingCart.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shoppingcart;
+public readonly record struct ShoppingCartId(Guid shoppingCartId) 
+{
+    public static ShoppingCartId NewId => new(Guid.NewGuid());
+    public static ShoppingCartId Empty => new(Guid.Empty);  
+};
+
+internal class ShoppingCart
+{
+    private readonly ShoppingCartId _shoppingCartId;
+    private readonly List<Item> _items = [];
+    private static readonly object _inventoryLock = new();
+
+    public ShoppingCart()
+    {
+        _shoppingCartId = ShoppingCartId.NewId;
+    }
+
+    public ShoppingCartId ShoppingCartId { get => _shoppingCartId; }
+
+    public List<Item> Items
+    {
+        get
+        {           
+            return new List<Item>(_items);
+        }     
+    }
+
+    public decimal TotalPrice
+    {
+        get
+        {            
+            return _items.Sum(item => item.Price);            
+        }      
+    }
+
+    public void AddItem(Item item)
+    {
+        if (item == null)
+            throw new ArgumentNullException(nameof(item), "Item cannot be null.");
+
+        lock (_inventoryLock)
+        {
+            if (item.QuantityInStock < 1)
+            {
+                Console.WriteLine($"Cannot add {item.ItemName} to cart. Item is out of stock!");
+            }
+            else
+            {
+                _items.Add(item);
+                item.QuantityInStock--;
+                Console.WriteLine($"{item.ItemName} X 1 added to cart.");
+            }
+        }
+    }
+
+    public void RemoveItem(Item item)
+    {
+        if (item == null)
+            throw new ArgumentNullException(nameof(item), "Item cannot be null.");
+
+        if (_items.Remove(item))
+        {
+            lock (_inventoryLock)
+            {
+                item.QuantityInStock++;
+                Console.WriteLine($"{item.ItemName} removed from cart.");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"{item.ItemName} not found in the cart.");
+        }
+    }
+
+    public void Checkout()
+    {
+        if (_items.Count == 0)
+        {
+            Console.WriteLine("\nCart is empty!");
+        }
+        else
+        {
+            Console.WriteLine($"\nThank you for your purchase of {TotalPrice:C} and not paying");
+            _items.Clear();
+        }
+    }
+
+    public void DisplayCartItems()
+    {        
+        Console.WriteLine("\nCart contents:\n");
+        foreach (var item in Items)
+        {
+            Console.WriteLine($"{item.ItemName} {item.Price:C}");
+        }
+        Console.WriteLine($"\nTotal Price: {TotalPrice:C}");
+    }
+}
+
